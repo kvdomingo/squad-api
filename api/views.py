eebe8ece3d0d0589fffd431a8b1ff175d1959040
Kenneth V. Domingo <hello@kvdomingo.xyz>
2021-06-12 03:06:11 +0800

    return only future events

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
+from django.utils import timezone
 from rest_framework.generics import ListAPIView
 from .serializers import *
 
 
 class EventView(ListAPIView):
-    queryset = Event.objects.all()
+    queryset = Event.objects.filter(date__gte=timezone.now())
     serializer_class = EventSerializer
