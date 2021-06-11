279a4bdd7c8bfffca9a4316bb95ec0fbe1d49800
Kenneth V. Domingo <hello@kvdomingo.xyz>
2021-06-12 02:47:28 +0800

    revert old datetime field

--- api/models.py
+++ api/models.py
@@ -4,8 +4,7 @@ from django.db import models
 class Event(models.Model):
     group = models.CharField(max_length=64)
     name = models.CharField(max_length=256)
-    date = models.DateField()
-    time = models.TimeField(blank=True, null=True)
+    date = models.DateTimeField()
 
     class Meta:
         ordering = ['date']
