aba35b9edb5ee88ca8e4aedd6add6678e3aadba1
Kenneth Domingo <hello@kvdomingo.xyz>
2021-10-08 19:46:17 +0800

    move admin route to root

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,15 @@
+from django.conf import settings
 from django.contrib import admin
 from .models import *
 
+admin.site.site_header = 'Schedule API Admin'
+admin.site.site_title = 'Schedule API Admin'
+admin.site.index_title = 'Admin'
+
+if settings.PYTHON_ENV == 'production':
+    admin.site.site_url = 'https://schedule.kvdstudio.app'
+else:
+    admin.site.site_url = 'http://localhost:3000'
 
 admin.site.register(Bias)
 admin.site.register(DiscordUser)
